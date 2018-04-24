1e632a16c954b191730ff0cf320f7d774b676559
Maddie <madwester@gmail.com>
2018-04-24 21:50:20 +1000

    testing again

---
+++ build/about.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="stylesheet" href="/compontents/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <title>Include Her | Where Female Developer Meets</title>
+  </head>
+  <body class="bodyAbout">
+    <header>
+      <nav class="navbar navbar-light navbar-expand-lg">
+        <a class="navbar-brand" href="index.html"><img src="images/include-logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-align-right navigationSymbol"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Forum</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Meet</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">About<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Connect</a>
+              </li>
+          </ul>
+          <button type="button" class="btn navBtn">Register/Login</button>
+        </div>
+      </nav>
+    </header>
+    <div class="feature featureAbout">
+    </div>
+    <main>  
+      <div class="container">
+        <div class="contentAbout">
+          <h1>Who are we?</h1>
+          <p>Include her is an online community where people with the same interest of coding can socialize, 
+            learn and inspire each other. It doesn't matter if you are a girl, a boy or another gender, 
+            anyone is welcome but unlikely many other coding communities, most of our members are girls. 
+            And we love that, because we need to inspire more girls to code. 
+            <br><br>
+            Include her was founded by Madeleine Westerstrom during her last semester of university. Madeleine studied 
+            a bachelor of IT and was struggling to find other girls into coding, this is where the idea of Include her came from. 
+            <br><br>
+            Include her is a place where no question in our forum is a stupid question, our members are excited about helping each 
+            other, whatever level of coding you're at. To make it easier for our members to meet up and make friends, we have a 
+            social section where anyone can arrange meetups, small as large. 
+            <br><br>
+            Include her always aims to improve and add features our members miss. If you have some feedback, suggestions or questions. 
+            Please don't hesitate to get in touch. <a href="#">connect ></a></p>
+        </div>
+    </div>  
+  </main>
+    <footer>
+      <div class="flexRow">
+        <div class="itemFoot">
+          <img src="images/include-logo.png">
+        </div>
+        <div class="itemFoot align-right">
+          <ul class="socialMediaList">
+            <li><i class="fab fa-instagram"></i></li>
+            <li><i class="fab fa-facebook-f"></i></li>
+            <li><i class="fab fa-google"></i></li>
+          </ul>
+        </div>
+      </div>
+      <hr> 
+      <div class="flexRow">
+        <div class="itemFoot">
+          <p>Copyright &copy;	2017. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="/compontents/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="stylesheet" href="/compontents/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <title>Include Her | Where Female Developer Meets</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-light navbar-expand-lg">
+        <a class="navbar-brand" href="index.html"><img src="images/include-logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-align-right navigationSymbol"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Forum</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Meet</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Connect</a>
+              </li>
+          </ul>
+          <button type="button" class="btn navBtn">Register/Login</button>
+        </div>
+      </nav>
+    </header>
+    <div class="feature featureHome">
+      <h1>We love to speak about code.<br>Welcome in!</h1>
+      <button type="button" class="btn featureBtn">Join us!</button>
+    </div>
+    <main>    <div class="container">
+        <div class="flexRow">
+          <div class="homepageItem large blue">
+            <p>In our social section we want you to arrange meet up with friends and new friends. 
+              We think coding together is the best way to code. A meetup can be small or large, whatever you prefer!</p>
+              <a href="#">Meet<i class="fa fa-angle-double-right"></i></a>
+          </div>
+          <div class="homepageItem small pink">
+            <p>Include her is an online community where anyone who is interested in coding is welcome. Read more about our story.</p>
+              <a href="about.html">About us<i class="fa fa-angle-double-right"></i></a>
+          </div>
+        </div>
+        <div class="flexRow">
+          <div class="homepageItem small gray">
+            <p>Have you got any questions? Please don't hestitate to get in touch.</p>
+              <a href="#">Connect<i class="fa fa-angle-double-right"></i></a>
+          </div>
+          <div class="homepageItem large yellow">
+            <p>Everyone knows being stuck is a part of coding, and it can be exhausting. In our forum our members are extremly 
+              helpful to solve your issues. </p>
+              <a href="#">Forum<i class="fa fa-angle-double-right"></i></a>
+          </div>
+        </div>
+    </div>
+  </main>
+    <footer>
+      <div class="flexRow">
+        <div class="itemFoot">
+          <img src="images/include-logo.png">
+        </div>
+        <div class="itemFoot align-right">
+          <ul class="socialMediaList">
+            <li><i class="fab fa-instagram"></i></li>
+            <li><i class="fab fa-facebook-f"></i></li>
+            <li><i class="fab fa-google"></i></li>
+          </ul>
+        </div>
+      </div>
+      <hr> 
+      <div class="flexRow">
+        <div class="itemFoot">
+          <p>Copyright &copy;	2017. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="/compontents/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
